56370cf80b8d2483866043ad7dd399ed1630fd59
E1480 <113873052+E1480@users.noreply.github.com>
2024-06-16 04:09:18 +0000

    First Commit, Base Website.

---
+++ E1480.Library/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+  <nav><ul>
+    <li><a href="#">icon</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Library</a></li>
+  </ul></nav>
+  <main></main>
+</template>
+
+<style scoped>
+
+</style>
